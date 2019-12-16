166da5845b1d536b97c02b7c4a989827086147e6
王贵源 <wangguiyuan@chinarecrm.com.cn>
2019-12-16 10:03:08 +0800

    :white_check_mark: 测试一下sql tpl

--- demo/src/main/java/org/nutz/demo/controller/TestController.java
+++ demo/src/main/java/org/nutz/demo/controller/TestController.java
@@ -2,6 +2,9 @@ package org.nutz.demo.controller;
 
 import org.nutz.dao.Dao;
 import org.nutz.dao.SqlManager;
+import org.nutz.dao.Sqls;
+import org.nutz.dao.sql.Sql;
+import org.nutz.lang.random.R;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,5 +23,15 @@ public class TestController {
     @GetMapping("sqls")
     public Result<SqlManager> sql() {
         return Result.success(dao.sqls());
+    }
+
+    @GetMapping("tpl")
+    public Result<Integer> tpl() {
+        Sql sql = dao.sqls().create("test.tpl");
+        sql.vars().set("a", R.random(1, 10));
+        sql.params().set("b", R.random(1, 10));
+        sql.setCallback(Sqls.callback.integer());
+        dao.execute(sql);
+        return Result.success(sql.getInt());
     }
 }
