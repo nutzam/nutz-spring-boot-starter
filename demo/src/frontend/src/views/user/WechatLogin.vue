93e90a9ff080052b6b92d19fb4970a498f6412af
Kerbores <kerbores@gmail.com>
2021-10-25 08:28:39 +0800

    add: 前端登录逻辑
    Signed-off-by: Kerbores <kerbores@gmail.com>

--- demo/src/frontend/src/views/user/WechatLogin.vue
+++ demo/src/frontend/src/views/user/WechatLogin.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>微信授权登录</div>
+  <div v-if="socialLoginBindDTO.openid">
+    <!-- 此处显示绑定表单-->
+    <input name="mobile" type="text" v-model="socialLoginBindDTO.mobile" />
+    <input name="code" type="text" v-model="socialLoginBindDTO.code" />
+    <button type="submit" @click="bind">绑定</button>
+  </div>
+  <div v-else>正在进行微信登录检查....</div>
 </template>
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
@@ -8,18 +14,37 @@ import qs from "qs";
   components: {},
 })
 export default class WechatLogin extends Vue {
+  socialLoginBindDTO: wechat.TieSocialLoginUserDataObject = {
+    channel: "MP",
+    code: "",
+    openid: "",
+    mobile: "",
+  };
+  state!: string;
   mounted(): void {
     const code = qs.parse(location.search.substr(1)).code;
-    console.log(code);
-    //TODO 根据code调用后台的接口,如果返回token就直接登录成功返回进来之前的页面,如果返回openid就进行绑定操作
+    const state = qs.parse(location.search.substr(1)).state;
+
+    this.state = state + "";
     this.$api.wechatApi.socialLogin.oauth(
       "WECHAT_SCAN",
       { code: code + "" },
       ({ data }) => {
         console.log(data); //token
+        //TODO 将token放入状态管理,然后根据state或者是进入此页面前保存的状态跳回原来的页面(恢复登录前现场)
       },
       (data) => {
-        console.log(data); // openid 显示绑定页面
+        this.socialLoginBindDTO.openid = data;
+      }
+    );
+  }
+
+  bind(): void {
+    this.$api.wechatApi.socialLogin.bind(
+      this.socialLoginBindDTO,
+      ({ data }) => {
+        console.log(data); //到这里的就是一定拿到token了
+        //TODO 将token放入状态管理,然后根据state或者是进入此页面前保存的状态跳回原来的页面(恢复登录前现场)
       }
     );
   }
