099e0a53f021c6e09f22922bfaf432cbf69a6d1e
kerbores <kerbores@riemann.tech>
2022-08-31 18:31:27 +0800

    :bookmark: 2.6.11
    Signed-off-by: kerbores <kerbores@riemann.tech>

--- src/main/java/org/nutz/spring/boot/json/NutzJsonMessageConverterAutoConfiguration.java
+++ src/main/java/org/nutz/spring/boot/json/NutzJsonMessageConverterAutoConfiguration.java
@@ -2,8 +2,11 @@ package org.nutz.spring.boot.json;
 
 import java.util.TimeZone;
 
+import javax.annotation.PostConstruct;
+
 import org.nutz.json.Json;
 import org.nutz.json.JsonFormat;
+import org.nutz.json.handler.JsonLocalDateLikeHandler;
 import org.nutz.lang.Strings;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
@@ -17,55 +20,58 @@ import org.springframework.http.converter.HttpMessageConverter;
  *
  */
 @Configuration
-@ConditionalOnClass({Json.class})
+@ConditionalOnClass({ Json.class })
 @EnableConfigurationProperties(NutzJsonAutoConfigurationProperties.class)
 public class NutzJsonMessageConverterAutoConfiguration {
 
-    @SuppressWarnings("deprecation")
-    @Bean
-    @ConditionalOnExpression("${nutz.json.enabled:true}")
-    public HttpMessageConverter<Object> springBootNutzJsonHttpMessageConverter(
-                                                                               NutzJsonAutoConfigurationProperties properties) {
-        JsonFormat format = null;
-        if (properties.getMode() != null) {// 直接模式设置
-            switch (properties.getMode()) {
-            case COMPACT:
-                format = JsonFormat.compact();
-                break;
-            case FORLOOK:
-                format = JsonFormat.forLook();
-                break;
-            case FULL:
-                format = JsonFormat.full();
-                break;
-            case NICE:
-                format = JsonFormat.nice();
-                break;
-            case TIDY:
-                format = JsonFormat.tidy();
-                break;
-            default:
-                format = JsonFormat.compact();
-                break;
-            }
-        } else {
-            format = Json.fromJson(JsonFormat.class, Json.toJson(properties));
-        }
-        if (Strings.isNotBlank(properties.getActived())) {
-            format.setActived(properties.getActived());
-        }
-        if (Strings.isNotBlank(properties.getLocked())) {
-            format.setLocked(properties.getLocked());
-        }
-        if (Strings.isNotBlank(properties.getDateFormat())) {
-            format.setDateFormat(properties.getDateFormat());
-        }
-        if (Strings.isNotBlank(properties.getTimeZone())) {
-            format.setTimeZone(TimeZone.getTimeZone(properties.getTimeZone()));
-        }
-        return new SpringBootNutzJsonMessageConverter().setFormat(format)
-                                                       .setIgnoreType(properties.getIgnoreType())
-                                                       .setIgnoreTypes(properties.getIgnoreTypes())
-                                                       .setignoreUris(properties.getIgnoreUris());
-    }
+	@PostConstruct
+	public void init() {
+		Json.addTypeHandler(new JsonLocalDateLikeHandler());
+	}
+
+	@SuppressWarnings("deprecation")
+	@Bean
+	@ConditionalOnExpression("${nutz.json.enabled:true}")
+	public HttpMessageConverter<Object> springBootNutzJsonHttpMessageConverter(
+			NutzJsonAutoConfigurationProperties properties) {
+		JsonFormat format = null;
+		if (properties.getMode() != null) {// 直接模式设置
+			switch (properties.getMode()) {
+			case COMPACT:
+				format = JsonFormat.compact();
+				break;
+			case FORLOOK:
+				format = JsonFormat.forLook();
+				break;
+			case FULL:
+				format = JsonFormat.full();
+				break;
+			case NICE:
+				format = JsonFormat.nice();
+				break;
+			case TIDY:
+				format = JsonFormat.tidy();
+				break;
+			default:
+				format = JsonFormat.compact();
+				break;
+			}
+		} else {
+			format = Json.fromJson(JsonFormat.class, Json.toJson(properties));
+		}
+		if (Strings.isNotBlank(properties.getActived())) {
+			format.setActived(properties.getActived());
+		}
+		if (Strings.isNotBlank(properties.getLocked())) {
+			format.setLocked(properties.getLocked());
+		}
+		if (Strings.isNotBlank(properties.getDateFormat())) {
+			format.setDateFormat(properties.getDateFormat());
+		}
+		if (Strings.isNotBlank(properties.getTimeZone())) {
+			format.setTimeZone(TimeZone.getTimeZone(properties.getTimeZone()));
+		}
+		return new SpringBootNutzJsonMessageConverter().setFormat(format).setIgnoreType(properties.getIgnoreType())
+				.setIgnoreTypes(properties.getIgnoreTypes()).setignoreUris(properties.getIgnoreUris());
+	}
 }
